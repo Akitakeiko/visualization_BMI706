40053a2a7b126bc587d3243472c8c7e22b1f281d
Cheryl Gu <cgu@hsph.harvard.edu>
2024-03-05 22:21:09 -0500

    -test

--- app.py
+++ app.py
@@ -15,11 +15,7 @@ st.set_page_config(
 
 
 @st.cache_data
-def load_data():
-    combined_df= pd.read_csv('https://raw.githubusercontent.com/Akitakeiko/visualization_BMI706/main/Data/combined_dfall.csv?token=GHSAT0AAAAAACOCXHGSHQQGSCNSFILX42HUZPHZWIA', index_col = 0)
-    return combined_df
-
-df = load_data()
+df = pd.read_csv('https://raw.githubusercontent.com/Akitakeiko/visualization_BMI706/main/Data/combined_dfall.csv?token=GHSAT0AAAAAACOCXHGSHQQGSCNSFILX42HUZPHZWIA', index_col = 0)
 
 st.write("## Temporal HPV cases and cohort sizes by Countries")
 year = st.slider("Year", min_value=2010, max_value=2030, value=2021)
