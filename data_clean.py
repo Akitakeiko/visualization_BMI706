f339a27225922aafa2a5a46b7506bfd5d6ea7622
Cheryl Gu <cgu@hsph.harvard.edu>
2024-03-06 02:48:32 -0500

    -updated df2

--- data_clean.py
+++ data_clean.py
@@ -5,6 +5,16 @@ def combined_df():
     df = pd.read_csv('/Users/akitakeiko/visualization_BMI706/data/combined_dfall.csv', index_col = 0)
     return df
 
-def hpv_df():
-    df2 = pd.read_csv("/Users/akitakeiko/visualization_BMI706/data/hpv_past_results.csv", index_col = 0)
+def hpv_df(hpv_past, hpv_2020):
+    hpv_past = pd.read_csv("/Users/akitakeiko/visualization_BMI706/data/hpv_past_results.csv", index_col = 0)
+    hpv_2020 = pd.read_csv("/Users/akitakeiko/visualization_BMI706/data/hpv_2020s_results.csv", index_col = 0)
+    id_vars = ['region', 'income_group', 'year', 'assumption_type']
+    value_vars = ['coverage', 'cancer_prevented', 'deaths_prevented', 'possible_cancer_cases', 'possible_cancer_deaths']
+    
+    hpv_past_melted = df_one.melt(id_vars=id_vars, value_vars=value_vars, var_name='metric', value_name='value')
+    hpv_2020_melted = df_two.melt(id_vars=id_vars, value_vars=value_vars, var_name='metric', value_name='value')
+    
+    # Combine the melted DataFrames
+    df2 = pd.concat([hpv_past_melted, hpv_2020_melted])
     return df2
+
