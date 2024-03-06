2bbd78f44282bc455ab8f5dc03ac0b1b6b5a2b6a
Cheryl Gu <cgu@hsph.harvard.edu>
2024-03-06 01:25:40 -0500

    -move read in data to data_clean and import

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
+from data_clean import load_data
+df = load_data()
 
 alt.data_transformers.disable_max_rows(); 
 
@@ -13,12 +15,6 @@ st.set_page_config(
     page_icon = 'img/hpv.png'
 )
 
-@st.cache_data
-def load_data():
-    df = pd.read_csv('/Users/akitakeiko/visualization_BMI706/Data/combined_dfall.csv', index_col = 0)
-    return df
-
-df = load_data()
 st.write("## Temporal HPV cases and cohort sizes by Countries")
 year = st.slider("Year", min_value=2010, max_value=2030, value=2021)
 subset = df[df["year"] == year]
