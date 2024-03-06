d3b337e85a0bb6be1a1f391d1bcf1ec0eb335386
Cheryl Gu <cgu@hsph.harvard.edu>
2024-03-06 12:17:43 -0500

    -test

--- process.py
+++ process.py
@@ -222,7 +222,7 @@ st.altair_chart(stacked_bar_chart, use_container_width=True)
 #map_background
 
 from map import return_world_map
-HPV_cases_map = return_world_map(df4, df4, year)
+HPV_cases_map = return_world_map(df4, subset, year)
 
 st.altair_chart(HPV_cases_map, use_container_width=True)
 
