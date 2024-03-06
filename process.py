96469703a884502bfb08b6314bf75e5ea39721f3
Cheryl Gu <cgu@hsph.harvard.edu>
2024-03-06 18:40:52 -0500

    -test

--- process.py
+++ process.py
@@ -66,7 +66,7 @@ st.write('### Explore spatial and temporal HPV cases, income group and vaccinati
 year= st.slider('Year', min_value=2010, max_value=2030, value=2020)
 subset = df3[df3["year"] == year]
 
-countries_default = [
+""" countries_default = [
     "Austria",
     "United Kingdom",
     "Brazil",
@@ -77,7 +77,7 @@ countries_default = [
 ]
 countries_name =  df["country_name"].unique()
 countries = st.multiselect("Countries", options = countries_name, default = countries_default) 
-subset = subset[subset["country_name"].isin(countries)]
+subset = subset[subset["country_name"].isin(countries)] """
 
 from map import return_world_map, return_income_map
 cases_map = return_world_map(df4,year)
