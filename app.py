d1b36f0b63f6edb9fb4e948b45b4f4693cb997ad
Cheryl Gu <cgu@hsph.harvard.edu>
2024-03-05 20:11:10 -0500

    -updated country and year selection

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ df = load_data()
 
 st.write("## Temporal HPV cases and cohort sizes by Countries")
 year = st.slider("Year", min_value=2010, max_value=2030, value=2021)
-subset = df[df["Year"] == year]
+subset = df[df["year"] == year]
