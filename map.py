c42619ed316b7c89e79e5d14ef2820515c1c3950
Cheryl Gu <cgu@hsph.harvard.edu>
2024-03-06 14:05:44 -0500

    -test

--- map.py
+++ map.py
@@ -78,55 +78,35 @@ def return_world_map(df_cleaned, selected_year):
 
 
 
-def return_world_map(df_cleaned, selected_year):
-    # Filter data for the selected year
-    df_year = df_cleaned[df_cleaned['year'] == selected_year]
+def return_world_map(df_cleaned):
+    case_scale = alt.Scale(domain=[df_cleaned['possible_cancer_cases'].min(), df_cleaned['possible_cancer_cases'].max()], scheme='oranges')
+    rate_color = alt.Color(field="possible_cancer_cases", type="quantitative", scale=case_scale)
 
-    # If there's no data after filtering, we can't proceed
-    if df_year.empty:
-        # Display a text message over the background map
-        return alt.Chart(source).mark_geoshape().encode(
-            tooltip=None
-        ).properties(
-            title=f'No HPV cases data available for {selected_year}',
-            width=width,
-            height=height
-        ).project(
-            type='equirectangular'
-        )
-
-    # Calculate min and max values for the current year to set the scale
-    min_cases = df_year['possible_cancer_cases'].min()
-    max_cases = df_year['possible_cancer_cases'].max()
-
-    # Define the color scale based on possible_cancer_cases
-    cases_scale = alt.Scale(domain=[min_cases, max_cases], scheme='oranges')
-
-    # Create the base of the map using the world geometry
-    chart_base_map = alt.Chart(source).mark_geoshape(
-    ).encode(
-        color=alt.Color('properties.possible_cancer_cases:Q', scale=cases_scale, legend=alt.Legend(title="HPV Cancer Cases"))
-    ).transform_lookup(
-        lookup='id',
-        from_=alt.LookupData(df_year, 'country-code', ['possible_cancer_cases'])
-    ).project(
-        type='equirectangular'
+    chart_base = alt.Chart(source
     ).properties(
         width=width,
         height=height
+    ).project(project
+    ).transform_lookup(
+        lookup="id",
+        from_=alt.LookupData(df_cleaned, "country-code", ['Country', 'income_group', 'possible_cancer_cases']),
     )
-
-    # Adding tooltip for interactivity
-    chart_cases = chart_base_map.encode(
-        tooltip=['properties.possible_cancer_cases:Q']
+    chart_case = chart_base.mark_geoshape().encode(
+    color= rate_color,
+    tooltip=["possible_cancer_cases:Q", "Country:N"]
+    ).transform_lookup(
+    lookup='id',
+    from_=alt.LookupData(df_cleaned, 'country-code', ['Country','possible_cancer_cases'])
     ).properties(
-        title=f'HPV cases worldwide in {selected_year}'
+    title='HPV cases Worldwide'
     )
     
-    cases_map = alt.vconcat(background + chart_cases
+    cases_map = alt.vconcat(background+chart_case
     ).resolve_scale(
         color = 'independent'
-    )
-
+    ) 
+             
     return cases_map
-
+    
+    
+     
