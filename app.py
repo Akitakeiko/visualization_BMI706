2adb98e68416481ed9ae0f239dfb1f3cb24e6087
Cheryl Gu <cgu@hsph.harvard.edu>
2024-03-05 19:54:03 -0500

    minor error fixed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import streamlit as st
 
 @st.cache_data
-def load_data()
+def load_data():
     mixed_df= pd.read_csv('https://raw.githubusercontent.com/Akitakeiko/visualization_BMI706/main/Data/combined_dfall.csv?token=GHSAT0AAAAAACOCXHGS4LMCJZZR5KPR3TH6ZPHXDOQ', index_col = 0)
     return mixed_df
 
