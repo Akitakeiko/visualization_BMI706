645c0fc6813ef65ad70bc5db11dc95293d634800
Cheryl Gu <cgu@hsph.harvard.edu>
2024-03-06 02:49:43 -0500

    -minor error fixed

--- data_clean.py
+++ data_clean.py
@@ -5,7 +5,7 @@ def combined_df():
     df = pd.read_csv('/Users/akitakeiko/visualization_BMI706/data/combined_dfall.csv', index_col = 0)
     return df
 
-def hpv_df(hpv_past, hpv_2020):
+def hpv_df():
     hpv_past = pd.read_csv("/Users/akitakeiko/visualization_BMI706/data/hpv_past_results.csv", index_col = 0)
     hpv_2020 = pd.read_csv("/Users/akitakeiko/visualization_BMI706/data/hpv_2020s_results.csv", index_col = 0)
     id_vars = ['region', 'income_group', 'year', 'assumption_type']
