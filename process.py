279a683364dfa5117f9f849555b90e364496ca08
Cheryl Gu <cgu@hsph.harvard.edu>
2024-03-06 00:37:34 -0500

    -change read in to raw data url

--- process.py
+++ process.py
@@ -14,7 +14,7 @@ import os
 
 @st.cache
 def load_data():
-    df = pd.read_csv("combined_dfall.csv")
+    df = pd.read_csv("https://raw.githubusercontent.com/Akitakeiko/visualization_BMI706/main/Data/combined_dfall.csv?token=GHSAT0AAAAAACOCXHGSI6BPZBCW5EFDC2OIZPIB4IA.csv")
     
     return df
 
