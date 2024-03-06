4efc7b09a8b6e09f9c3124aaa0b137a7c5b3a502
Cheryl Gu <cgu@hsph.harvard.edu>
2024-03-05 19:52:26 -0500

    -modify reading in data

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ import numpy as np
 import streamlit as st
 
 @st.cache_data
-df = pd.read_csv('https://raw.githubusercontent.com/Akitakeiko/visualization_BMI706/main/Data/combined_dfall.csv?token=GHSAT0AAAAAACOCXHGS4LMCJZZR5KPR3TH6ZPHXDOQ', index_col = 0)
+def load_data()
+    mixed_df= pd.read_csv('https://raw.githubusercontent.com/Akitakeiko/visualization_BMI706/main/Data/combined_dfall.csv?token=GHSAT0AAAAAACOCXHGS4LMCJZZR5KPR3TH6ZPHXDOQ', index_col = 0)
+    return df
+
+df = load_data()
 st.write("## Age-specific cancer mortality rates")
