9267806f43072d64d7f966b46b8e02573f9ef6d7
Cheryl Gu <cgu@hsph.harvard.edu>
2024-03-05 19:37:59 -0500

    -added few lines to test streamlit

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import altair as alt
 import pandas as pd
+import numpy as np
 import streamlit as st
 
+@st.cache_data
+df = pd.read_csv('https://raw.githubusercontent.com/Akitakeiko/visualization_BMI706/main/Data/combined_dfall.csv?token=GHSAT0AAAAAACOCXHGS4LMCJZZR5KPR3TH6ZPHXDOQ', index_col = 0)
+st.write("## Age-specific cancer mortality rates")
