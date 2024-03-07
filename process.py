b5ca49d8d2cc06b3e645a83392cb1e3b0b13171a
Cheryl Gu <cgu@hsph.harvard.edu>
2024-03-06 19:11:40 -0500

    -test

--- process.py
+++ process.py
@@ -64,7 +64,7 @@ st.write('### Explore spatial and temporal HPV cases, income group and vaccinati
 
 # Slider for year
 year= st.slider('Year', min_value=2010, max_value=2030, value=2020)
-subset3 = df3[df3["year"] == year]
+subset = df3[df3["year"] == year]
 
 countries_default = [
     "Austria",
@@ -88,7 +88,7 @@ st.altair_chart(income_map, use_container_width=True)
 st.altair_chart(cases_map, use_container_width=True)
 
 
-bar_chart_cohort = alt.Chart(subset3).mark_bar(color='steelblue').encode(
+bar_chart_cohort = alt.Chart(subset).mark_bar(color='steelblue').encode(
     x=alt.Y('sum(cohort_size):Q', title='Sum of cohort size'),
     y=alt.X('country_name:N', title='Country', sort='-x'),
     tooltip=['country_name:N', 'sum(cohort_size):Q']
@@ -96,7 +96,7 @@ bar_chart_cohort = alt.Chart(subset3).mark_bar(color='steelblue').encode(
     title='Cohort Size by Country'
 )
 
-bar_chart_cases = alt.Chart(subset3).mark_bar(color='pink').encode(
+bar_chart_cases = alt.Chart(subset).mark_bar(color='pink').encode(
     x=alt.Y('sum(possible_cancer_cases):Q', title='Sum of HPV cancer cases'),
     y=alt.X('country_name:N', title='Country', sort='-x'),
     tooltip=['country_name:N', 'sum(possible_cancer_cases):Q']
