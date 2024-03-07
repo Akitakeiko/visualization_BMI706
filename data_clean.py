bc439e580a0ab6280efffcd83c6a3fb786498283
Cheryl Gu <cgu@hsph.harvard.edu>
2024-03-06 20:35:48 -0500

    - remove coverage from metric

--- data_clean.py
+++ data_clean.py
@@ -9,7 +9,7 @@ def hpv_df():
     hpv_past = pd.read_csv("https://raw.githubusercontent.com/Akitakeiko/visualization_BMI706/main/Data/hpv_past_results.csv", index_col = 0)
     hpv_2020 = pd.read_csv("https://raw.githubusercontent.com/Akitakeiko/visualization_BMI706/main/Data/hpv_2020s_results.csv", index_col = 0)
     id_vars = ['region', 'income_group', 'year', 'assumption_type']
-    value_vars = ['coverage', 'cancer_prevented', 'deaths_prevented', 'possible_cancer_cases', 'possible_cancer_deaths']
+    value_vars = ['cancer_prevented', 'deaths_prevented', 'possible_cancer_cases', 'possible_cancer_deaths']
     
     hpv_past_melted = hpv_past.melt(id_vars=id_vars, value_vars=value_vars, var_name='metric', value_name='value')
     hpv_2020_melted = hpv_2020.melt(id_vars=id_vars, value_vars=value_vars, var_name='metric', value_name='value')
