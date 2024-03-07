8ffa8c1c7cfa97e6bf194f2635cf50ff794b9685
Cheryl Gu <cgu@hsph.harvard.edu>
2024-03-06 20:42:10 -0500

    -add country_code.py

---
+++ Data/add_countrycode.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+country_df = pd.read_csv('https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/country_codes.csv', dtype = {'conuntry-code': str})
+
+df=pd.read_csv("Data/combined_cohort.csv")
+
+
+
+df_merged = pd.merge(df, country_df[['Country', 'country-code']], on='Country', how='left')
+
+df_cleaned = df_merged.dropna(subset=['country-code'])
+
+df_cleaned.to_csv('Data/country-code_cohort.csv', index=False)
